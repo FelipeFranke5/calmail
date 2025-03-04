553403bb06400181d745979886f07ac12a481021
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-04 13:47:02 -0300

    misc: inclusão de uma nova exception

---
+++ src/main/java/br/com/frankefelipe5/calmail/api/exception/AIResponseAcessNotGrantedException.java
@@ -0,0 +1,7 @@
+package br.com.frankefelipe5.calmail.api.exception;
+
+public class AIResponseAcessNotGrantedException extends RuntimeException {
+    public AIResponseAcessNotGrantedException(String message) {
+        super(message);
+    }
+}
